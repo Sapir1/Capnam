671bf631d0dc732f8b0c02c3b69982b77b93ec33
A-Small-Being <hazelmeehan123@gmail.com>
2019-03-11 19:23:33 +1300

    Added Wall Class

---
+++ Wall.py
@@ -0,0 +1,13 @@
+# Wall class
+class Wall:
+    def __init__(self,gameWindow):
+        # self.wCoords = wCoords
+        self.wCoords = [{'x': 9,'y': 9},
+                        {'x': 9, 'y': 10}] #Player coordinates
+        self.gameWindow = gameWindow
+
+    def drawWall(self):
+        """
+        Tell gameWindow to draw 'wall' on screen
+        """
+        self.gameWindow.drawCircle(self.wCoords,self.gameWindow.WHITE)
